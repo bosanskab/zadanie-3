3cd4e42ac28bf40b61856c5587c650d43451faf6
bosanskab <bosanska.bar@gmail.com>
2017-04-26 20:10:55 +0200

    Add files via upload
    generátor

--- generator.c
+++ generator.c
@@ -13,23 +13,14 @@ void generator (char *file, int n2){
 	
  	in= open("argv[1]", O_RDONLY);
  	out=open("avg[1]_kopia", O_WRONLY|O_CREAT, S_IRUSR|S_IWUSR);
-
-//	if(in==NULL){
-//		printf("%s", strerror(errno));
-//		return;
-//	}
-//	if(out==NULL){
-//		printf("%s", strerror(errno));
-//		return;
-//	}
 	
 	for (j=1;j<n2;j++){
 		n1=10+rand()%11;
         F[0]=(unsigned char)n1;
 	 for (i=1;i<n1;i++){
 	   F[i]=rand()%RAND_MAX/1024;
-		 printf("%.2lf\n",	F[i]);
-	 		 buf=((float)rand())/(float)RAND_MAX/1024;
+		 printf("%.1lf\n",	F[i]);
+	 		 buf=((float)rand())/((float)RAND_MAX/1024);
 	  	 		write(out,&buf,1);
 		}
 
@@ -38,44 +29,18 @@ void generator (char *file, int n2){
 	close(out);
 }
 
-void analyzator (char *file, int n){
-	char *buf;
-	int i,length=0,pocet=0,suma=1,priemer=0;
-	int in,out,prvok;
-	
-	in= open("argv[1]_kopia", O_RDONLY);
-//	if(in== NULL){
-//		printf("%s", strerror(errno));
-//		return;
-//	}
- 	while (read(in,&buf,1)==1)
- 	length++;
- 	if (sizeof(in)==sizeof(unsigned char))
- 	pocet++;
-   	exit(0);
-   	
- 	for (i=1;i<length;i++){
- 	 prvok=*buf+i;
- 	 suma=+prvok;
- 		if (sizeof(buf)==sizeof(unsigned char)){
-	    priemer=suma/i;
-	    suma=0;
-		}
-	}
- 	printf("celkova dlzka: %d",length);
- 	printf("pocet postup.: %d",pocet);
- 	
-	close(in);
-}
-
-
-
 int main(int argc, int **argv){
-	srand(time(NULL));
+	int *memory;
 	char *file =(char *)argv[1];
-//	int n=atoi(argv[2]);
-    int n=10;
-	generator (file,n);
-	analyzator (file,n);
+	int n=atoi((const char*)argv[2]);
+    
+	srand(time(NULL));
+	memory=malloc(20*n*sizeof(float));
+		if (memory==NULL)
+	  printf("malloc zlyhal\n");
 
+	generator (file,n);
+	void free (int *memory){
+		free(memory);
+	}
 }
