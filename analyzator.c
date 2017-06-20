41d7d3518852369e0e715e62b5764904d40044fa
bosanskab <bosanska.bar@gmail.com>
2017-06-20 21:54:12 +0200

    Add files via upload

--- analyzator.c
+++ analyzator.c
@@ -1,7 +1,7 @@
-  #include<string.h>           
- #include<stdio.h>
+  #include <string.h>           
+ #include <stdio.h>
  #include <time.h>
- #include<stdlib.h>
+ #include <stdlib.h>
  #include <unistd.h>
  #include <sys/stat.h>
  #include <fcntl.h>
@@ -11,25 +11,25 @@
 	int i,length=0,pocet=0,suma=1,priemer=0,m=1,pocetprvkov=0;
  	int in,out,prvok;
  	
- 	in= open("argv[1]_kopia", O_RDONLY);
+ 	in= open(file, O_RDONLY);
   	while (read(in,&buf,1)==1)
   		length++;
   		
   	if (sizeof(in)==sizeof(unsigned char)){
   		scanf("%c",*buf);
-  		&buf=&buf+sizeof(unsigned char);
+  		buf=buf+sizeof(unsigned char);
 	}
-  	else scanf("%lf",*buf);
+  		else scanf("%lf",*buf);
   	
-	&buf=&buf+sizeof(float);
+	buf=buf+sizeof(float);
     pocet++;
-   	exit(0);
     	
   	for (i=1;i<length*sizeof(float);i=+sizeof(unsigned char)){
   		prvok=*buf+i;
   		printf("postup.: %d",prvok);
  		suma=+prvok;
  		pocetprvkov++;
+ 		
 		if (sizeof(buf)==sizeof(unsigned char)){
 			printf("\n");
  	   		p[m]=suma/pocetprvkov;
@@ -38,8 +38,10 @@
  	    	priemer=+p[m];
  	    	m++;
  	    	}
+ 	    	
  	priemer=priemer/m;
  	}
+ 	
   	printf("celkova dlzka: %d",length);
   	printf("pocet postup.: %d",pocet);
   	printf("priemer postup.: %d",priemer);
@@ -53,11 +55,6 @@
 	int n=atoi((const char*)argv[2]);
     
     file =(char *)argv[1];
-	srand(time(NULL));
-	file=malloc(20*n*sizeof(float));
-		if (file==NULL)
-	  printf("malloc zlyhal\n");
 
 	analyzator (file,n);
-	free(file);
 }
